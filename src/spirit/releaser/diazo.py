9e4ea81e699bb7dbdbc4ceb84fb8ee69e7e9a139
Thomas Massmann <thomas.massmann@it-spir.it>
2017-10-03 17:33:45 +0200

    Refactoring of release_diazo method.

--- src/spirit/releaser/diazo.py
+++ src/spirit/releaser/diazo.py
@@ -64,25 +64,32 @@ def release_diazo(data):
 
     manifest_file = os.path.join(diazo_folder, 'manifest.cfg')
     has_manifest = os.path.exists(manifest_file)
-    if has_manifest and config.has_option(SECTION, OPTION_TITLE_UPDATE):
-        try:
-            config.getboolean(SECTION, OPTION_TITLE_UPDATE)
-        except ValueError:
-            pass
-        else:
-            if utils.ask(
-                'Add version number to the theme title in exported zip file?',
-                default=True,
-            ):
-                manifest = ConfigParser()
-                manifest.read(manifest_file)
-                version = pkg_resources.get_distribution(package_name).version
-                title = manifest.get('theme', 'title')
-                manifest.set('theme', 'title', ' '.join([title, version]))
-                with open(manifest_file, 'wb') as configfile:
-                    manifest.write(configfile)
+    if has_manifest:
+        if config.has_option(SECTION, OPTION_TITLE_UPDATE):
+            _update_title(config, manifest_file, package_name)
+
     create_zipfile(tmp_folder, data.get('workingdir'), package_name)
     shutil.rmtree(tmp_folder)
+
+
+def _update_title(config, manifest_file, package_name):
+    """Update the title of the theme."""
+    try:
+        config.getboolean(SECTION, OPTION_TITLE_UPDATE)
+    except ValueError:
+        return
+    else:
+        if utils.ask(
+            'Add version number to the theme title in exported zip file?',
+            default=True,
+        ):
+            manifest = ConfigParser()
+            manifest.read(manifest_file)
+            version = pkg_resources.get_distribution(package_name).version
+            title = manifest.get('theme', 'title')
+            manifest.set('theme', 'title', ' '.join([title, version]))
+            with open(manifest_file, 'wb') as configfile:
+                manifest.write(configfile)
 
 
 def create_zipfile(src, dist, package_name):
